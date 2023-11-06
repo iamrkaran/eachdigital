ead8d81009749749ef3c59209974d7d2a6ddac26
aditya <adityareddy1451@gmail.com>
2023-11-06 16:17:53 +0530

    updated extraordinary profiles section

--- src/components/(landing)/sections/ExtraordinaryProfiles.tsx
+++ src/components/(landing)/sections/ExtraordinaryProfiles.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
 import React from "react";
 
 const profiles = [
+  {
+    imageUrl: "/images/profile/1.png",
+  },
   {
     imageUrl: "/images/profile/2.png",
   },
@@ -14,25 +16,56 @@ const profiles = [
   {
     imageUrl: "/images/profile/5.png",
   },
+  {
+    imageUrl:"/images/profile/6.png",
+  }
 ];
+
+const videoUrl = "/videos/extraordinary.mp4"; // Add the URL to your video here
+const imageWidth = 450;
+const imageHeight = 800;
 
 const ExtraordinaryProfiles = () => {
   return (
     <section className="bg-primary-100 py-16">
       <div className="container mx-auto text-center">
-      <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold mb-4">Extraordinary Profiles</h2>
+        <h2 className="text-4xl font-extrabold mb-8">Extraordinary Profiles</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {profiles.map((profile, index) => (
-            <div key={index} className="bg-white rounded-lg p-6 shadow-md">
-              <Image
-                width={500}
-                height={500}
+            <div key={index} >
+              <img
                 src={profile.imageUrl}
                 alt="profile"
+                width={imageWidth}
+                height={imageHeight}
+                style={{
+                  width: `${imageWidth}px`,
+                  height: `${imageHeight}px`,
+                }}
                 className="w-full h-full rounded-lg mx-auto mb-4"
               />
             </div>
           ))}
+        </div>
+        <div className="bg-white rounded-lg shadow-md"
+          style={{
+            width:"900px",
+            height:"550px",
+            padding:"16px",
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            margin: "50px auto",
+          }}
+        >
+          <video
+            controls
+            width="100%"
+            height="auto"
+            src={videoUrl}
+          >
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </section>
